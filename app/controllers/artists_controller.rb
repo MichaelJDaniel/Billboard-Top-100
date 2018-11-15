0eec5e6ef419841bed9f501b6a00a8f44287b133
MichaelJDaniel <soldbymichaeld@gmail.com>
2018-11-15 15:11:00 -0700

    add controller and model

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,13 +1,44 @@
 class ArtistsController < ApplicationController
+  before_action :set_board , except: [:index, :new, :create]
   def index
+  @artist = Artist.new
   end
 
   def show
   end
 
   def new
+    @artist = Artist.new
   end
 
   def edit
+    # render :edit
   end
-end
+
+  def create
+    @artist = Artist.new[artist_params]
+    if @artist.save
+      redirect_to artists_path
+    else
+      render :new
+    end
+  end
+
+
+  def update
+    if @artist.update[artist_params]
+      redirect_to artists_path
+    else
+      render :edit
+  end
+
+
+
+  private
+  
+  def set_artist
+    @artist = Artist.find(params[:id])
+  end
+
+  def board_params
+    params.require(:artist).permit(:name)
